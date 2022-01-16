8f44bc52f43e7c6730d4fdcbdca3c6e82d8e6577
yssupriya <97824125+yssupriya@users.noreply.github.com>
2022-01-16 22:21:20 +0530

    Add files via upload

---
+++ final project/LockedMeMain.java
@@ -0,0 +1,12 @@
+package com.lockedme;
+public class LockedMeMain {
+public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		menuOptions.printWelcomeScreen("Locker", "anilkumar");
+		
+		HandleOption.handleWelcomeScreenInput();
+	}
+}
